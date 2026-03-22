7dfefe1bc381e436f602f850982b017ab4f018a3
Marcos <marcosa@iesb.edu.br>
2026-03-21 21:49:08 -0300

    feat: conclui pratica01. Fecha #ID

--- aulas/aula01/src/main.js
+++ aulas/aula01/src/main.js
@@ -1,1 +1,14 @@
 import './style.css'
+
+/*
+validar o formulario com as regras:
+- matricula > 0 e obrigatoria
+- senha tamanho > 6 caracteres e obrigatoria
+*/
+
+const matricula = document.querySelector('#matricula');
+const senha = document.querySelector('#senha');
+const botao = document.querySelector('#botao');
+
+botao .addEventListener('')
+const cabeçalho = document.querySelector();
