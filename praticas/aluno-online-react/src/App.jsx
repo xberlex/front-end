6bd9888f59831605157a8496132f4efe352b84b4
Marcos <marcosa@iesb.edu.br>
2026-05-12 10:13:05 -0300

    resolvendo conflitos da pratica05

--- praticas/aluno-online-react/src/App.jsx
+++ praticas/aluno-online-react/src/App.jsx
@@ -1,29 +1,31 @@
-import "./App.css";
+import './App.css';
 import { useState } from 'react';
-import Boletos from "./pages/Boletos";
-import Dashboard from "./pages/Dashboard";
-import Faltas from "./pages/Faltas";
-import Login from "./pages/Login";
-import Notas from "./pages/Notas";
-import Requerimentos from "./pages/Requerimentos";
-import Dashboard from "./pages/Dashboard";
-import ListagemDeBoletos from "./pages/Listagemdeboletos";
-import ListagemDeFaltas from "./pages/Listagemdefaltas";
-import ListagemDeNotas from "./pages/Listagemdenotas";
-import ListagemDeRequerimentos from "./pages/Listagemderequerimentos";
+
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import Notas from './pages/Notas';
+import Faltas from './pages/Faltas';
+import Boletos from './pages/Boletos';
+import Requerimentos from './pages/Requerimentos';
+
 
 function App() {
   const [pagina, setPagina] = useState(0);
 
-  switch(pagina) {
-    case 1: return <Dashboard navegaPara={setPagina}/>
-    case 2: return <Notas navegaPara={setPagina}/>
-    case 3: return <Faltas navegaPara={setPagina}/>
-    case 4: return <Boletos navegaPara={setPagina}/>
-    case 5: return <Requerimentos navegaPara={setPagina}/>
-    default: return <Login navegaPara={setPagina}/>
+  switch (pagina) {
+    case 1:
+      return <Dashboard navegaPara={setPagina} />;
+    case 2:
+      return <Notas navegaPara={setPagina} />;
+    case 3:
+      return <Faltas navegaPara={setPagina} />;
+    case 4:
+      return <Boletos navegaPara={setPagina} />;
+    case 5:
+      return <Requerimentos navegaPara={setPagina} />;
+    default:
+      return <Login navegaPara={setPagina} />;
   }
-  return <Dashboard/>;
 }
 
 export default App;
