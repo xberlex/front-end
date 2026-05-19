e97d584dcda9ddd4153e54a3b81734fdb05238a6
Marcos <marcosa@iesb.edu.br>
2026-05-19 13:47:15 -0300

    continuação da aula04

--- aulas/aula04/src/contexts/AuthContext.jsx
+++ aulas/aula04/src/contexts/AuthContext.jsx
@@ -24,7 +24,7 @@ function AuthProvider({ children }) {
 }
 
 function useAuth() {
-    return useContext(AuthContext);
+    return useContext(AuthContext);     
 } 
 
 export { AuthProvider, useAuth };
